e11579edc1144c73ae41d08b6afbcea07137222c
SetKrul <setkrul@gmail.com>
2015-09-02 14:26:28 +0300

    rtl

--- app/src/main/java/com/crmc/ourcity/adapter/PhoneBookListAdapter.java
+++ app/src/main/java/com/crmc/ourcity/adapter/PhoneBookListAdapter.java
@@ -78,7 +78,7 @@ public class PhoneBookListAdapter extends BaseAdapter {
         }
 
         public void setData(PhoneBook _item) {
-            ivArrowPhoneBook_PBF.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.event_arrow_right,
+            ivArrowPhoneBook_PBF.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.event_arrow,
                     Image.darkenColor(0.2)));
             tvCategory_PBF.setText(_item.categoryName.trim());
         }
