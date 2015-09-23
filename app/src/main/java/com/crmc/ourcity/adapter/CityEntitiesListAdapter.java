9fe57f46c01b29593e8873ce6282ae259d8f250b
SetKrul <setkrul@gmail.com>
2015-09-23 17:17:58 +0300

    fix

--- app/src/main/java/com/crmc/ourcity/adapter/CityEntitiesListAdapter.java
+++ app/src/main/java/com/crmc/ourcity/adapter/CityEntitiesListAdapter.java
@@ -69,7 +69,7 @@ public class CityEntitiesListAdapter extends BaseAdapter {
         final ImageView ivArrowEvent;
         final ImageView ivCall;
         final ImageView ivSendMail;
-        final ImageView ivCall_Mobile;
+//        final ImageView ivCall_Mobile;
         final View view;
         final Context mContext;
         private int position;
@@ -80,10 +80,10 @@ public class CityEntitiesListAdapter extends BaseAdapter {
             ivArrowEvent = (ImageView) _view.findViewById(R.id.ivArrowCityEntities_CEF);
             ivCall = (ImageView) _view.findViewById(R.id.ivCall_CEF);
             ivSendMail = (ImageView) _view.findViewById(R.id.ivMail_CEF);
-            ivCall_Mobile = (ImageView) _view.findViewById(R.id.ivCall_Mobile_CEF);
+//            ivCall_Mobile = (ImageView) _view.findViewById(R.id.ivCall_Mobile_CEF);
             ivCall.setOnClickListener(this);
             ivSendMail.setOnClickListener(this);
-            ivCall_Mobile.setOnClickListener(this);
+//            ivCall_Mobile.setOnClickListener(this);
             this.view = _view;
             this.mContext = _context;
             _view.setTag(this);
@@ -94,7 +94,7 @@ public class CityEntitiesListAdapter extends BaseAdapter {
             ivArrowEvent.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.event_arrow, Image
                     .darkenColor(0.2)));
             if (!TextUtils.isEmpty(_item.phoneNumber)) {
-                ivCall.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.phone2,
+                ivCall.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.phone,
                         Image.darkenColor(0.2)));
 
                 ivCall.setVisibility(View.VISIBLE);
@@ -109,14 +109,14 @@ public class CityEntitiesListAdapter extends BaseAdapter {
             } else {
                 ivSendMail.setVisibility(View.GONE);
             }
-            if (!TextUtils.isEmpty(_item.mobileNumber)) {
-                ivCall_Mobile.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.phone,
-                        Image.darkenColor(0.2)));
-
-                ivCall_Mobile.setVisibility(View.VISIBLE);
-            } else {
-                ivCall_Mobile.setVisibility(View.GONE);
-            }
+//            if (!TextUtils.isEmpty(_item.mobileNumber)) {
+//                ivCall_Mobile.setImageDrawable(Image.setDrawableImageColor(mContext, R.drawable.phone,
+//                        Image.darkenColor(0.2)));
+//
+//                ivCall_Mobile.setVisibility(View.VISIBLE);
+//            } else {
+//                ivCall_Mobile.setVisibility(View.GONE);
+//            }
             if (TextUtils.isEmpty(_item.entityName)) {
                 title.setVisibility(View.GONE);
             } else {
@@ -152,15 +152,15 @@ public class CityEntitiesListAdapter extends BaseAdapter {
                                 Toast.LENGTH_SHORT).show();
                     }
                     break;
-                case R.id.ivCall_Mobile_CEF:
-                    try {
-                        mContext.startActivity(Intent.createChooser(IntentUtils.getIntentCall(getItem(position)
-                                .mobileNumber), mContext.getResources().getString(R.string.call_hint)));
-                    } catch (ActivityNotFoundException e) {
-                        Toast.makeText(mContext, mContext.getResources().getString(R.string.app_no_call_client),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    break;
+//                case R.id.ivCall_Mobile_CEF:
+//                    try {
+//                        mContext.startActivity(Intent.createChooser(IntentUtils.getIntentCall(getItem(position)
+//                                .mobileNumber), mContext.getResources().getString(R.string.call_hint)));
+//                    } catch (ActivityNotFoundException e) {
+//                        Toast.makeText(mContext, mContext.getResources().getString(R.string.app_no_call_client),
+//                                Toast.LENGTH_SHORT).show();
+//                    }
+//                    break;
             }
         }
     }
