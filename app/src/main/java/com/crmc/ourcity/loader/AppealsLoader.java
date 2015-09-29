d42acb43dbe59453e3ce331cb8f2de23c9eb0ba7
SetKrul <setkrul@gmail.com>
2015-09-29 15:24:33 +0300

    fix

--- app/src/main/java/com/crmc/ourcity/loader/AppealsLoader.java
+++ app/src/main/java/com/crmc/ourcity/loader/AppealsLoader.java
@@ -2,13 +2,11 @@ package com.crmc.ourcity.loader;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.text.TextUtils;
 
 import com.crmc.ourcity.global.Constants;
 import com.crmc.ourcity.rest.RestClientApi;
 import com.crmc.ourcity.rest.api.CityApi;
 import com.crmc.ourcity.rest.responce.appeals.WSResult;
-import com.crmc.ourcity.utils.Image;
 import com.crmc.ourcity.utils.SPManager;
 
 import java.io.UnsupportedEncodingException;
@@ -42,15 +40,15 @@ public class AppealsLoader extends BaseLoader<WSResult> {
 
         try {
             wsResult = api.getAppeals(route, new TypedByteArray("application/json", json.getBytes("UTF-8")));
-            if (wsResult.getResultObjects() != null) {
-                for (int i = 0; i < wsResult.getResultObjects().size(); i++) {
-                    if (!TextUtils.isEmpty(wsResult.getResultObjects().get(i).AttachedFiles)) {
-                        wsResult.getResultObjects().get(i).AttachedFiles = Image.compressBitmap(Image
-                                .convertBase64ToBitmap(wsResult.getResultObjects().get(i).AttachedFiles));
-
-                    }
-                }
-            }
+//            if (wsResult.getResultObjects() != null) {
+//                for (int i = 0; i < wsResult.getResultObjects().size(); i++) {
+//                    if (!TextUtils.isEmpty(wsResult.getResultObjects().get(i).AttachedFiles)) {
+//                        wsResult.getResultObjects().get(i).AttachedFiles = Image.compressBitmap(Image
+//                                .convertBase64ToBitmap(wsResult.getResultObjects().get(i).AttachedFiles));
+//
+//                    }
+//                }
+//            }
         } catch (RetrofitError | UnsupportedEncodingException _e) {
             wsResult = null;
         }
