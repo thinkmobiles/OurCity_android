20be6ac7c1dee439491cc61cc7f5860c0b43da1d
andrewMistetskii <alexakhmatov@hotmail.com>
2015-10-01 18:05:55 +0300

    fix

--- app/src/main/java/com/crmc/ourcity/dialog/DialogActivity.java
+++ app/src/main/java/com/crmc/ourcity/dialog/DialogActivity.java
@@ -119,7 +119,7 @@ public class DialogActivity extends BaseFragmentActivity implements OnActionDial
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-//        finish();
+        finish();
     }
 
     @Override
