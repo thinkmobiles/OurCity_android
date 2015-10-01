00277bc44a51d6a3ee7fd019ed7150657f14547e
andrewMistetskii <alexakhmatov@hotmail.com>
2015-10-01 15:23:57 +0300

    1.Changed the field title: http://screencast.com/t/IYGku5fss5 to “תאור”
    2.increased speed of ticker

--- app/src/main/java/com/crmc/ourcity/ticker/Ticker.java
+++ app/src/main/java/com/crmc/ourcity/ticker/Ticker.java
@@ -42,7 +42,7 @@ public class Ticker extends TextView implements Animator.AnimatorListener, View.
 
         setOnClickListener(this);
         mAimator = ObjectAnimator.ofFloat(this, "translationX", -1 * width, width);
-        mAimator.setDuration(7000);
+        mAimator.setDuration(5000);
         mAimator.addListener(this);
         mAimator.setRepeatCount(ValueAnimator.INFINITE);
 
